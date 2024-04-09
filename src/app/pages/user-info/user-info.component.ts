eb7fff7305beaab89d8b98500098c1ba26744f96
Gabriel Milhomem <gabrielpacmil@gmail.com>
2024-04-09 14:23:53 -0300

    Using owner-form as editing user form

--- src/app/pages/user-info/user-info.component.ts
+++ src/app/pages/user-info/user-info.component.ts
@@ -3,15 +3,15 @@ import { OwnerStateService } from '../../shared/services/owner-state.service';
 import { OwnerService } from '../../shared/services/owener.service';
 import { Owner } from '../../models/owner';
 import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
-import { formatDate, NgIf } from '@angular/common';
-import { FormsModule, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
 import { Router } from '@angular/router';
 import { HttpErrorResponse } from '@angular/common/http';
+import { OwnerFormComponent } from '../../shared/components/owner-form/owner-form.component';
 
 @Component({
   selector: 'app-user-info',
   standalone: true,
-  imports: [NavbarComponent, NgIf, FormsModule, ReactiveFormsModule],
+  imports: [NavbarComponent, NgIf, OwnerFormComponent],
   templateUrl: './user-info.component.html',
   styleUrl: './user-info.component.scss'
 })
@@ -19,31 +19,15 @@ export class UserInfoComponent {
   public error?: string;
   public user?: Owner;
   public editing: boolean;
-  public editUserForm = this.formBuilder.group(
-    {
-      name: ['', [Validators.required]],
-      email: ['', [Validators.required, Validators.email]],
-      cpf: ['', [Validators.required]],
-      birthDate: [formatDate(new Date(), 'yyyy-MM-dd', 'en'), [Validators.required]],
-    }
-  )
 
   constructor(
     private ownerStateService: OwnerStateService,
-    private formBuilder: FormBuilder,
     private ownerService: OwnerService,
     private router: Router
   ){
     this.ownerStateService.user.subscribe(user => {
       this.user = user;
-      this.editUserForm = this.formBuilder.group(
-        {
-          name: [user.name, Validators.required],
-          email: [user.email, [Validators.email, Validators.required]],
-          cpf: [user.cpf, Validators.required],
-          birthDate: [this.formatDateFromDD_MM_YY(user.birthDate), Validators.required],
-        }
-    )})
+    })
     this.editing = false;
   }
 
@@ -56,18 +40,9 @@ export class UserInfoComponent {
     this.editing = false;
   }
 
-  updateUser(){
-    if(!this.user || this.editUserForm.invalid)
+  updateUser(updateUser: Owner){
+    if(!this.user)
       return;
-
-    let formValue = this.editUserForm.value;
-
-    let updateUser: Owner = {
-      name: formValue.name? formValue.name : "",
-      email: formValue.email? formValue.email: "",
-      cpf: formValue.cpf? formValue.cpf: "",
-      birthDate: this.formatDateFromYYYY_MM_DD(formValue.birthDate)
-    };
 
     if(this.ownerService.areUsersEqual(updateUser, this.user)){
       this.editing = false;
@@ -102,25 +77,5 @@ export class UserInfoComponent {
           error: (errorResponse: HttpErrorResponse) => {
             alert("Error while deleting user: " + errorResponse.error.message);
           }})
-  }
-
-  private formatDateFromYYYY_MM_DD(strDate?: string | null) : string {
-    if(!strDate)
-      return "";
-
-    let year = strDate.slice(0, 4);
-    let month = strDate.slice(5, 7);
-    let day = strDate.slice(-2);
-    return `${day}/${month}/${year}`;
-  }
-
-  private formatDateFromDD_MM_YY(strDate?: string | null) : string {
-    if(!strDate)
-      return "";
-
-    let year = strDate.slice(-4);
-    let month = strDate.slice(3, 5);
-    let day = strDate.slice(0,2);
-    return `${year}-${month}-${day}`;
   }
 }
