8e6ef6c3d5017f8f5951db4bab89e4612622da6f
Gabriel Milhomem <gabrielpacmil@gmail.com>
2024-04-08 23:59:33 -0300

    Adding guard to vehicles and user-info routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,39 @@ import { VehiclesComponent } from './pages/vehicles/vehicles.component';
 import { UnauthorizedComponent } from './pages/unauthorized/unauthorized.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { UserInfoComponent } from './pages/user-info/user-info.component';
+import { apiGuard } from './guards/guard.guard';
 
 export const routes: Routes = [
-    {path:'', redirectTo: 'home', pathMatch: 'full'},
-    {path: 'home', title:"home", component: HomeComponent},
-    {path: 'vehicles', title:'vehicles', component: VehiclesComponent},
-    {path: 'user-info', title: 'user info', component: UserInfoComponent},
-    {path: 'unauthorized', title: 'unauthorized', component: UnauthorizedComponent},
-    {path: '**', title: "not found", component: NotFoundComponent}
+    {
+        path:'',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        title:"home",
+        component: HomeComponent
+    },
+    {
+        path: 'vehicles',
+        title:'vehicles',
+        component: VehiclesComponent,
+        canActivate: [apiGuard]
+    },
+    {
+        path: 'user-info', 
+        title: 'user info', 
+        component: UserInfoComponent, 
+        canActivate: [apiGuard]
+    },
+    {
+        path: 'unauthorized',
+        title: 'unauthorized',
+        component: UnauthorizedComponent
+    },
+    {
+        path: '**',
+        title: "not found",
+        component: NotFoundComponent
+    }
 ];
---
+++ src/app/guards/guard.guard.ts
@@ -0,0 +1,34 @@
+import { CanActivateFn } from '@angular/router';
+import { OwnerStateService } from '../shared/services/owner-state.service';
+import { Injectable, inject } from '@angular/core';
+import { Owner } from '../models/owner';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+class PermissionService {
+
+  private user?: Owner;
+  constructor(
+    private ownerStateService: OwnerStateService,
+    private router: Router
+  ){
+    this.ownerStateService.user.subscribe(user => {
+      this.user = user;
+    });
+  }
+
+  canActivate(){
+    if(this.user)
+      return true;
+
+    this.router.navigate(['/unauthorized']);
+    return false;
+  }
+}
+
+
+export const apiGuard: CanActivateFn = (route: any, state: any) => {
+  return inject(PermissionService).canActivate();
+};
