b396137d6279301940bc9e5738b89aaf1cb96651
Gabriel Milhomem <gabrielpacmil@gmail.com>
2024-04-09 23:59:53 -0300

    Adding type selection

--- src/app/pages/vehicles/add-vehicle/add-vehicle.component.ts
+++ src/app/pages/vehicles/add-vehicle/add-vehicle.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { NgIf, NgFor } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { VehicleType } from '../../../models/api-fipe/models';
 
 @Component({
   selector: 'app-add-vehicle',
   standalone: true,
-  imports: [],
+  imports: [NgIf, NgFor, FormsModule],
   templateUrl: './add-vehicle.component.html',
   styleUrl: './add-vehicle.component.scss'
 })
 export class AddVehicleComponent {
 
+  objectKeys = Object.keys;
+
+  @Output() cancel: EventEmitter<any> = new EventEmitter(); 
+
+  vehicleTypes = VehicleType;
+  vehicleType: string = "";
+  vehicleBrand: string = "";
+  vehicleModel: string = "";
+  vehicleYear: string = "";
+
+  emitCancel(){
+    this.cancel.emit();
+  }
+
+  addVehicle(){
+
+  }
 }
